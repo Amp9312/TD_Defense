3c864445384829abee194149cc805c939387d581
Anthony Hake <lvlup97@gmail.com>
2020-12-07 22:05:07 -0500

    Finished 2.0 Script for moving camera around
    Uses WASD to move camera. Also utilized Vector3 and object coordinates to tell camera where the edges of the tilemap are.

---
+++ Scripts/CameraMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Last Edited 12/2/2020
+ * Edited by Amp
+ * 
+ * Note - uppercase are properties while lowercase are fields
+ */
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float cameraSpeed = 0;
+
+    private float xMax;
+    private float yMin;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * cameraSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * cameraSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * cameraSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * cameraSpeed * Time.deltaTime);
+        }
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, 0, xMax), Mathf.Clamp(transform.position.y, yMin, 0), -10);
+
+    }
+
+    //This function identifies the edge of the generated tilemap and prevents the player from scrolling any farther
+    public void SetLimits(Vector3 maxTile)
+    {
+        Vector3 wp = Camera.main.ViewportToWorldPoint(new Vector3(1, 0));
+
+        xMax = maxTile.x - wp.x;
+        yMin = maxTile.y - wp.y;
+    }
+}
